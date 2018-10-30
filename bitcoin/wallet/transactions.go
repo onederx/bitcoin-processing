2abb75a84b99bd50f7c5042127ceae21a159cb7d
Daniil Sigalov <dansmbox@gmail.com>
2018-10-30 22:33:34 +0300

    Transaction type: add more fields and methods
    Add enum field Direction that determines whether it is an incoming
    or outgoing payment. For now, only "receive" transactions will
    produce "incoming" direction and only "send" will produce
    "outgoing", any other value will be reflected as "unknown".
    Also, add reportedConfirmations field that stores a number of
    confirmations which already was communicated to listeners via
    events mechanism.
    Also, add constructor newTransaction that makes Transaction struct
    based on btcjson.ListTransactionsResult and method
    updateFromFullTxInfo that updates block hash and number of
    confirmations received from GetTransaction method.

--- bitcoin/wallet/transactions.go
+++ bitcoin/wallet/transactions.go
@@ -1,7 +1,18 @@
 package wallet
 
 import (
+	"log"
+
+	"github.com/btcsuite/btcd/btcjson"
 	"github.com/satori/go.uuid"
+)
+
+type TransactionDirection string
+
+const (
+	DIRECTION_INCOMING TransactionDirection = "incoming"
+	DIRECTION_OUTGOING                      = "outgoing"
+	DIRECTION_UNKNOWN                       = "unknown"
 )
 
 type Transaction struct {
@@ -11,6 +22,8 @@ type Transaction struct {
 	Confirmations int64                `json:"confirmations"`
 	Address       string               `json:"address"`
 	Direction     TransactionDirection `json:"direction"`
+
+	reportedConfirmations int64
 }
 
 func (tx *Transaction) update(other *Transaction) {
@@ -19,4 +32,37 @@ func (tx *Transaction) update(other *Transaction) {
 	}
 	tx.BlockHash = other.BlockHash
 	tx.Confirmations = other.Confirmations
+}
+
+func (tx *Transaction) updateFromFullTxInfo(other *btcjson.GetTransactionResult) {
+	if tx.Hash != other.TxID {
+		panic("Tx update called for transaction with other hash")
+	}
+	tx.BlockHash = other.BlockHash
+	tx.Confirmations = other.Confirmations
+}
+
+func newTransaction(btcNodeTransaction *btcjson.ListTransactionsResult) *Transaction {
+	var direction TransactionDirection
+	if btcNodeTransaction.Category == "receive" {
+		direction = DIRECTION_INCOMING
+	} else if btcNodeTransaction.Category == "send" {
+		direction = DIRECTION_OUTGOING
+	} else {
+		log.Printf(
+			"Warning: unexpected transaction category %s for tx %s",
+			btcNodeTransaction.Category,
+			btcNodeTransaction.TxID,
+		)
+		direction = DIRECTION_UNKNOWN
+	}
+
+	return &Transaction{
+		Hash:                  btcNodeTransaction.TxID,
+		BlockHash:             btcNodeTransaction.BlockHash,
+		Confirmations:         btcNodeTransaction.Confirmations,
+		Address:               btcNodeTransaction.Address,
+		Direction:             direction,
+		reportedConfirmations: -1,
+	}
 }
