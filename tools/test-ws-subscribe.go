5f09cdb03813cfc3685c1cc8bc851210397fb88a
Daniil Sigalov <dansmbox@gmail.com>
2018-11-03 14:48:16 +0300

    test program test-ws-subscribe: support seq
    Update manual testing program tools/test-ws-subscribe.go to
    make it subscribe from given seq. Cmdline arg -seq sets seq num to
    subscribe from, defaults to 0.

--- tools/test-ws-subscribe.go
+++ tools/test-ws-subscribe.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/url"
@@ -11,11 +12,22 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type sendMeMessagesFromSeqRequest struct {
+	StartSeq int `json:"seq"`
+}
+
 var addr = flag.String("addr", "localhost:8000", "http service address")
+var startSeq = flag.Int("seq", 0, "sequence number to get messages from")
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
+
+	seqRequest, err := json.Marshal(sendMeMessagesFromSeqRequest{*startSeq})
+
+	if err != nil {
+		panic(err)
+	}
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
@@ -30,6 +42,7 @@ func main() {
 	defer c.Close()
 
 	done := make(chan struct{})
+	c.WriteMessage(websocket.TextMessage, seqRequest)
 
 	go func() {
 		defer close(done)
