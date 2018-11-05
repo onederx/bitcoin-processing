8999926520370bd7d6876f2050410541b69b784b
Daniil Sigalov <dansmbox@gmail.com>
2018-11-05 17:57:53 +0300

    Add enum describing fee type
    The plan is to support two fee types: per kb rate (this is what
    Bitcoin Core supports out-of-the-box and uses automatically) and
    fixed - a constant amount of money.

---
+++ bitcoin/fee.go
@@ -0,0 +1,45 @@
+package bitcoin
+
+import (
+	"errors"
+)
+
+type FeeType int
+
+const (
+	FixedFee FeeType = iota
+	PerKBRateFee
+	InvalidFee
+)
+
+var feeTypeToStringMap map[FeeType]string = map[FeeType]string{
+	FixedFee:     "fixed",
+	PerKBRateFee: "per-kb-rate",
+}
+
+var stringToFeeTypeMap map[string]FeeType = make(map[string]FeeType)
+
+func init() {
+	for feeType, feeTypeStr := range feeTypeToStringMap {
+		stringToFeeTypeMap[feeTypeStr] = feeType
+	}
+}
+
+func (ft FeeType) String() string {
+	feeTypeStr, ok := feeTypeToStringMap[ft]
+	if ok {
+		return feeTypeStr
+	}
+	return "invalid"
+}
+
+func FeeTypeFromString(feeTypeStr string) (FeeType, error) {
+	ft, ok := stringToFeeTypeMap[feeTypeStr]
+
+	if ok {
+		return ft, nil
+	}
+	return InvalidFee, errors.New(
+		"Failed to convert string '" + feeTypeStr + "' to fee type",
+	)
+}
