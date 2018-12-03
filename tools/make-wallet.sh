33fc7b64685155b97ad693340e419d0334645199
Daniil Sigalov <dansmbox@gmail.com>
2018-12-04 00:16:54 +0300

    Remove deprecated manual test scripts from tools/
    Remove test shell scripts whose functionality is now implemented
    by command-line client in cmd/bitcoin-processing-client
    
    See #2

--- tools/make-wallet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -s -X POST http://127.0.0.1:8000/new_wallet --data '{
-    "user": "testuser",
-    "id": '$RANDOM'
-}' | python -m json.tool
--- tools/notify-wallet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s -X POST http://127.0.0.1:8000/notify-wallet
