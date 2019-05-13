ee63fba7f9a1f9e3bb3545d1ea625a52b498c331
Daniil Sigalov <dansmbox@gmail.com>
2019-05-14 02:34:52 +0300

    Use storage.Open for storage initialization
    Storages now accept *DB returned by storage.Open instead of
    connecting to database themselves - so, use this new convention
    in processing entrypoint.

--- cmd/bitcoin-processing/main.go
+++ cmd/bitcoin-processing/main.go
@@ -6,21 +6,23 @@ import (
 	"github.com/onederx/bitcoin-processing/bitcoin/wallet"
 	"github.com/onederx/bitcoin-processing/events"
 	"github.com/onederx/bitcoin-processing/settings"
+	"github.com/onederx/bitcoin-processing/storage"
 )
 
 func main() {
 	settings.ReadSettingsAndRun(func(loadedSettings settings.Settings) {
 		nodeAPI := nodeapi.NewNodeAPI(loadedSettings)
-		storageType := loadedSettings.GetStringMandatory("storage.type")
+		db := storage.Open(loadedSettings)
+
 		eventBroker := events.NewEventBroker(
 			loadedSettings,
-			events.NewEventStorage(storageType, loadedSettings),
+			events.NewEventStorage(db),
 		)
 		bitcoinWallet := wallet.NewWallet(
 			loadedSettings,
 			nodeAPI,
 			eventBroker,
-			wallet.NewStorage(storageType, loadedSettings),
+			wallet.NewStorage(db),
 		)
 		apiServer := api.NewServer(
 			loadedSettings.GetString("api.http.address"),
