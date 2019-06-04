2619e32225a3e931587f886b4c966f21d979c35a
Daniil Sigalov <dansmbox@gmail.com>
2019-06-04 05:01:21 +0300

    Add reliability integration tests for withdraw
    See #5

--- integrationtests/reliability_test.go
+++ integrationtests/reliability_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/jackc/pgx/pgproto3"
 
+	"github.com/onederx/bitcoin-processing/bitcoin"
 	"github.com/onederx/bitcoin-processing/bitcoin/wallet"
 	"github.com/onederx/bitcoin-processing/events"
 	"github.com/onederx/bitcoin-processing/integrationtests/testenv"
@@ -79,6 +80,36 @@ func TestDepositReliability(t *testing.T) {
 		for moment, momentName := range failureMoments {
 			runSubtest(t, failureName+"On"+momentName, func(t *testing.T) {
 				eventSeq = runDepositReliabilityCase(t, env, ctx, failure, moment, eventSeq)
+			})
+		}
+	}
+}
+
+func TestWithdrawReliability(t *testing.T) {
+	ctx := context.Background()
+	env, err := testenv.NewTestEnvironment(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = env.Start(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer env.Stop(ctx)
+	env.WaitForLoad()
+
+	eventSeq := 0
+
+	withdrawFailureMoments := make(map[failureMoment]string)
+
+	for _, m := range []failureMoment{FailureMomentTxWrite, FailureMomentEventWrite, FailureMomentHTTPSentSeqWrite} {
+		withdrawFailureMoments[m] = failureMoments[m]
+	}
+
+	for failure, failureName := range failureTypes {
+		for moment, momentName := range withdrawFailureMoments {
+			runSubtest(t, failureName+"On"+momentName, func(t *testing.T) {
+				eventSeq = runWithdrawReliabilityCase(t, env, ctx, failure, moment, eventSeq)
 			})
 		}
 	}
@@ -164,12 +195,138 @@ func runDepositReliabilityCase(t *testing.T, env *testenv.TestEnvironment, ctx c
 
 	<-failureDone
 
-	recoverable := restoreAfterFailure(t, env, mitm, ctx, failure, moment)
+	recoverable := restoreAfterFailure(t, env, mitm, ctx, failure, moment, false)
 
 	if !recoverable {
 		return 0
 	}
 
+	checkDepositCompletedNormally(t, env, tx, ourBalance, testDepositAmount)
+
+	return env.WebsocketListeners[0].LastSeq
+}
+
+func runWithdrawReliabilityCase(t *testing.T, env *testenv.TestEnvironment, ctx context.Context, failure failureType, moment failureMoment, eventSeq int) int {
+	depositBig := bitcoin.Must(bitcoin.BTCAmountFromStringedFloat("5"))
+
+	qtForConn := make(map[*pgmitm.Connection]queryType)
+	failureCount := 1
+
+	if failure == FailureConnFailureMultiple {
+		failureCount = 2
+	}
+
+	processingSettings := testenv.DefaultSettings
+	processingSettings.CallbackURL = env.CallbackURL
+
+	allowMITMUpstreamConnFailure := failure == FailureDBCrash
+
+	mitm := addPgMITMOrFail(env, &processingSettings, t, allowMITMUpstreamConnFailure)
+
+	defer mitm.Shutdown()
+
+	err := env.StartProcessing(ctx, &processingSettings)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer env.StopProcessing(ctx)
+	env.WaitForProcessing()
+	_, err = env.NewWebsocketListener(eventSeq + 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if len(env.WebsocketListeners) > 0 {
+			env.WebsocketListeners[0].Stop()
+			env.WebsocketListeners = env.WebsocketListeners[1:]
+		}
+	}()
+
+	// deposit some money so there is what to withdraw
+	ourBalance := getStableBalanceOrFail(t, env)
+	clientAccount := testGenerateClientWalletWithMetainfo(t, env, initialTestMetainfo, -1)
+	tx := testMakeDeposit(t, env, clientAccount.Address, depositBig, initialTestMetainfo)
+	checkDepositCompletedNormally(t, env, tx, ourBalance, depositBig)
+
+	mitm.AddClientMsgHandler(func(msg pgproto3.FrontendMessage, c *pgmitm.Connection) {
+		if parseMsg, ok := msg.(*pgproto3.Parse); ok {
+			qtForConn[c] = determineQueryTypeFromParseMsg(parseMsg)
+		}
+	})
+
+	failureDone := make(chan struct{})
+
+	mitm.AddClientMsgHandler(func(msg pgproto3.FrontendMessage, c *pgmitm.Connection) {
+		switch m := msg.(type) {
+		case *pgproto3.Query:
+			if moment == FailureMomentCommit && m.String == "COMMIT" {
+				causeFailure(t, env, c, mitm, failure, &failureCount, ctx)
+				if failureCount == 0 {
+					close(failureDone)
+					failureCount--
+				}
+			}
+		case *pgproto3.Bind:
+			qt := qtForConn[c]
+			switch {
+			case moment == FailureMomentTxWrite && qt == queryTxWrite:
+				fallthrough
+			case moment == FailureMomentEventWrite && qt == queryEventWrite:
+				fallthrough
+			case moment == FailureMomentLastSeenBlockHashWrite && qt == queryMetainfoWrite && string(m.Parameters[0]) == "last_seen_block_hash":
+				fallthrough
+			case moment == FailureMomentHTTPSentSeqWrite && qt == queryMetainfoWrite && string(m.Parameters[0]) == "last_http_sent_seq":
+				causeFailure(t, env, c, mitm, failure, &failureCount, ctx)
+				if failureCount == 0 {
+					close(failureDone)
+					failureCount--
+				}
+			}
+		}
+	})
+
+	ourBalance = getStableBalanceOrFail(t, env)
+	clientBalance := getStableClientBalanceOrFail(t, env)
+	withdrawAddress := getNewAddressForWithdrawOrFail(t, env)
+
+	req := &wallet.WithdrawRequest{
+		Address: withdrawAddress,
+		Amount:  withdrawAmountSmall,
+		Fee:     withdrawFee,
+	}
+
+	var resp *wallet.WithdrawRequest
+	withdrawDone := make(chan struct{})
+
+	go func() {
+		defer close(withdrawDone)
+		resp, err = env.ProcessingClient.Withdraw(req)
+	}()
+
+	<-failureDone
+
+	recoverable := restoreAfterFailure(t, env, mitm, ctx, failure, moment, true)
+
+	<-withdrawDone
+
+	switch {
+	case failure == FailureProcessingCrash && moment != FailureMomentHTTPSentSeqWrite && err == nil:
+		t.Fatal("Expected Withdraw to return error when processing crashed")
+	case failure != FailureProcessingCrash && err != nil:
+		t.Fatal(err)
+	}
+
+	if !recoverable {
+		return 0
+	}
+
+	checkWithdrawCompletedNormally(t, env, req, resp, clientBalance)
+
+	return env.WebsocketListeners[0].LastSeq
+}
+
+func checkDepositCompletedNormally(t *testing.T, env *testenv.TestEnvironment, tx *txTestData, ourBalance, amount bitcoin.BTCAmount) {
 	notification := env.GetNextCallbackNotificationWithTimeout(t)
 	tx.id = notification.ID
 	checkNotificationFieldsForNewDeposit(t, notification, tx)
@@ -187,15 +344,30 @@ func runDepositReliabilityCase(t *testing.T, env *testenv.TestEnvironment, ctx c
 	}
 	checkNotificationFieldsForNewDeposit(t, data, tx)
 
-	checkBalance(t, env, ourBalance, ourBalance+testDepositAmount)
+	checkBalance(t, env, ourBalance, ourBalance+amount)
 
 	tx.mineOrFail(t, env)
 
 	testDepositFullyConfirmed(t, env, tx)
 
-	checkBalance(t, env, ourBalance+testDepositAmount, ourBalance+testDepositAmount)
+	checkBalance(t, env, ourBalance+amount, ourBalance+amount)
+}
 
-	return env.WebsocketListeners[0].LastSeq
+func checkWithdrawCompletedNormally(t *testing.T, env *testenv.TestEnvironment, req, resp *wallet.WithdrawRequest, clientBalance bitcoin.BTCAmount) {
+	checkClientWithdrawRequest(t, resp, req)
+
+	tx := &txTestData{
+		id:      resp.ID,
+		address: req.Address,
+		amount:  withdrawAmountSmall,
+		fee:     withdrawFee,
+	}
+
+	expectedClientBalanceAfterWithdraw := clientBalance + withdrawAmountSmall - withdrawFee
+
+	testWithdrawNewTransaction(t, env, tx, clientBalance, expectedClientBalanceAfterWithdraw)
+	tx.mineOrFail(t, env)
+	testWithdrawFullyConfirmed(t, env, tx, expectedClientBalanceAfterWithdraw)
 }
 
 func determineQueryTypeFromParseMsg(parseMsg *pgproto3.Parse) queryType {
@@ -238,7 +410,7 @@ func causeFailure(t *testing.T, env *testenv.TestEnvironment, c *pgmitm.Connecti
 	}
 }
 
-func restoreAfterFailure(t *testing.T, env *testenv.TestEnvironment, mitm *pgmitm.PgMITM, ctx context.Context, failure failureType, moment failureMoment) bool {
+func restoreAfterFailure(t *testing.T, env *testenv.TestEnvironment, mitm *pgmitm.PgMITM, ctx context.Context, failure failureType, moment failureMoment, isWithdraw bool) bool {
 	switch failure {
 	case FailureProcessingCrash:
 		if env.Processing != nil {
@@ -250,7 +422,10 @@ func restoreAfterFailure(t *testing.T, env *testenv.TestEnvironment, mitm *pgmit
 		if err != nil {
 			t.Fatal(err)
 		}
-		recoverable := moment != FailureMomentHTTPSentSeqWrite
+
+		// for withdraw, crash at any moment between sending money and writing
+		// info about it to DB is unrecoverable
+		recoverable := !isWithdraw && moment != FailureMomentHTTPSentSeqWrite
 
 		if !recoverable {
 			handleUnrecoverableCrash(t, env, ctx)
