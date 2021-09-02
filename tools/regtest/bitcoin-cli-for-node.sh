230de66828c5191d36a44ce220a1671111af8a2a
asterite <asterite@seclab.cs.msu.ru>
2021-09-02 17:04:50 +0300

    tools/regtest: add -rpcwait flag to cli commands
    Add -rpcwait to bitcoin-cli invocatoions. It should ensure that
    bitcoin-cli will wait for API to become available if it is now
    available yet right now.

--- tools/regtest/bitcoin-cli-for-node.sh
+++ tools/regtest/bitcoin-cli-for-node.sh
@@ -2,4 +2,4 @@
 
 node=`basename "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"`
 
-docker-compose exec $node bitcoin-cli -regtest $@
+docker-compose exec $node bitcoin-cli -rpcwait -regtest $@
