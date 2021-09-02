8b8ad4f0ee39800d64bf3a7cd6f0546b149e8ace
asterite <asterite@seclab.cs.msu.ru>
2021-09-02 17:09:22 +0300

    tools/regtest: create wallets in bitcoin nodes
    In new Bitcoin Core versions, wallet is not auto-created on
    startup by default. So create it manually

--- tools/regtest/recreate-all.sh
+++ tools/regtest/recreate-all.sh
@@ -5,5 +5,8 @@ docker-compose down
 docker-compose up -d
 sleep 2
 echo "Will now mine some bitcoins"
+node1/bitcoin-cli -named createwallet wallet_name=default load_on_startup=true
+node2/bitcoin-cli -named createwallet wallet_name=default load_on_startup=true
+node3/bitcoin-cli -named createwallet wallet_name=default load_on_startup=true
 while ! ./gen-money.sh ; do echo "Trying to generate bitcoins..." && sleep 0.5; done
 echo "Done"
