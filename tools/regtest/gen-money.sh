05058d5c3c65ff8a9b5eafb12d05d6c9c075e94a
asterite <asterite@seclab.cs.msu.ru>
2021-09-02 17:07:12 +0300

    tools/regtest: remove trailing \r in gen-money.sh
    generatetoaddress command now returns a string terminated by \r\n
    and \r in it gets preserved when command output is saved to a
    variable. Which breaks subsequent usage of it in on ther commands.
    Remove that \r.

--- tools/regtest/gen-money.sh
+++ tools/regtest/gen-money.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-node2/bitcoin-cli generatetoaddress 3 `node2/bitcoin-cli getnewaddress`
+node2/bitcoin-cli generatetoaddress 3 `node2/bitcoin-cli getnewaddress | tr '\r' ' '`
 sleep 1
-node3/bitcoin-cli generatetoaddress 110 `node3/bitcoin-cli getnewaddress`
+node3/bitcoin-cli generatetoaddress 110 `node3/bitcoin-cli getnewaddress | tr '\r' ' '`
